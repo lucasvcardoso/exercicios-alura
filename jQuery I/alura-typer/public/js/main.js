5ab9ab5aa1e313539826be2bb1762e2042e839a0
Lucas Vieira Cardoso <lucas.v.cardoso@gmail.com>
2017-10-16 21:57:33 -0200

    Finalizada aula 3 do curso de jQuery.

--- jQuery I/alura-typer/public/js/main.js
+++ jQuery I/alura-typer/public/js/main.js
@@ -7,11 +7,26 @@ tamanhoFrase.text("numPalavras");
 var campo = $(".campo-digitacao");
 
 campo.on("input", function(){
-    var campoDigitacao = $(".campo-digitacao");
     //Para ler o VALOR de um campo de input, utilizamos val()
     var conteudo = campo.val();
     //Utilizamos a RegEx /\S+/ para separar por espaço + 1.
     var qtdPalavras = conteudo.split(/\S+/).length - 1;
     $("#contador-palavras").text(qtdPalavras);
     $("#contador-caracteres").text(conteudo.length);
+});
+
+var tempoRestante = $("#tempo-digitacao").text();
+//Podemos utilizar a função one() para executar o código do evento apenas na primeira vez que o evento ocorrer
+campo.one("focus", function(){
+    //Todo setInterval retorna seu próprio ID, podemos guardá-lo em uma variável.
+    var cronometroId = setInterval(function(){
+        if(tempoRestante > 0){
+            tempoRestante--;
+            $("#tempo-digitacao").text(tempoRestante);
+        }else{
+            campo.attr("disabled", true);
+            //Podemos utilizar este ID do setInterval para fazê-lo parar
+            clearInterval(cronometroId);
+        }
+    }, 1000);
 });
