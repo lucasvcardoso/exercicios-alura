fbbcb2819f843c250b9ccdf4a58e8a01c6da70e4
Lucas Vieira Cardoso <lucas.v.cardoso@gmail.com>
2017-11-05 02:10:59 -0200

    Finalizada aula 1 (Addressability) curso de Web API

---
+++ Web API/LojaAPI/LojaAPI/Controllers/CarrinhoController.cs
@@ -0,0 +1,24 @@
+﻿using LojaAPI.DAO;
+using LojaAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LojaAPI.Controllers
+{
+    public class CarrinhoController : ApiController
+    {
+        /*
+         Por convenção nomeia-se os métodos do controller de acordo com os verbos HTTP aos quais esses métodos responderão
+             */
+        public string Get(int id)
+        {
+            CarrinhoDAO dao = new CarrinhoDAO();
+            Carrinho carrinho = dao.Busca(1);
+            return carrinho.ToXml();
+        }
+    }
+}
--- Web API/LojaAPIClient/LojaAPIClient/Program.cs
+++ Web API/LojaAPIClient/LojaAPIClient/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,20 @@ namespace LojaAPIClient
     {
         static void Main(string[] args)
         {
+            string conteudo;
+            //HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://mocky.io/v2/52aaf5deee7ba8c70329fb7d");
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:57118/api/Carrinho/1");
+            request.Method = "GET";
+
+            WebResponse response = request.GetResponse();
+            using (Stream responseStream = response.GetResponseStream())
+            {
+                StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
+                conteudo = reader.ReadToEnd();
+            }
+
+            Console.Write(conteudo);
+            Console.Read();
         }
     }
 }
