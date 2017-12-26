09e008a715a12e61c192b88e436c39d166a66c5f
Lucas Vieira Cardoso <lucas.v.cardoso@gmail.com>
2017-12-26 10:29:11 -0200

    Finalizada aula 04 do curso React I

---
+++ React I/cdc-admin/src/componentes/BotaoSubmitCustomizado.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class BotaoSubmitCustomizado extends Component{
+
+  render(){
+    return(
+      <div className="pure-control-group">                                  
+        <label></label> 
+        <input type="submit" className="pure-button pure-button-primary" value={this.props.label} />    
+      </div>
+    );
+  }
+
+}
+
