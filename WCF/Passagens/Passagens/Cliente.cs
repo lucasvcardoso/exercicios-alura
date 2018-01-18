a7ea394c5bdf3f90b2e74d167b9bec193fc6f44c
Lucas Vieira Cardoso <lucas.cardoso@btgpactual.com>
2018-01-18 17:43:35 -0200

    Iniciado curso de WCF

---
+++ WCF/Passagens/Passagens/Cliente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Passagens
+{
+    [DataContract]
+    public class Cliente
+    {
+        //DataMemberAttribute informa ao WCF que esse atributo da classe deve trafegar pelo serviço
+        [DataMember]
+        public string Nome { get; set; }
+        [DataMember]
+        public string Cpf { get; set; }
+    }
+}
---
+++ WCF/Passagens/Passagens/ClienteService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Passagens
+{
+    public class ClienteService : IClienteService
+    {
+        public void Add(Cliente c)
+        {
+            ClienteDAO dao = new Passagens.ClienteDAO();
+            dao.Add(c);
+        }
+
+        public Cliente Buscar(string nome)
+        {
+            ClienteDAO dao = new Passagens.ClienteDAO();
+            return dao.Buscar(nome);
+        }
+    }
+}
