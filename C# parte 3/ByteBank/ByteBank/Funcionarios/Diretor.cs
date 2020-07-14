9e2add68207a0c0ae33ddc36417929cc48d8185f
Lucas Vieira de Oliveira Cardoso <lucas.v.cardoso@gmail.com>
2020-07-14 01:12:32 -0300

    Finalizada aula 3

--- C# parte 3/ByteBank/ByteBank/Funcionarios/Diretor.cs
+++ C# parte 3/ByteBank/ByteBank/Funcionarios/Diretor.cs
@@ -6,15 +6,21 @@ using System.Threading.Tasks;
 
 namespace ByteBank.Funcionarios
 {
-    public class Diretor
+    public class Diretor : Funcionario
     {
-        public string Nome { get; set; }
-        public string CPF { get; set; }
-        public double Salario { get; set; }
-
-        public double GetBonificacao()
+        public Diretor(string cpf) : base(5000, cpf)
         {
-            return Salario;
+            Console.WriteLine("Construtor de DIRETOR");
+        }
+
+        public override double GetBonificacao()
+        {
+            return Salario + base.GetBonificacao();
+        }
+
+        public override void AumentarSalario()
+        {
+            Salario *= 1.15;
         }
     }
 }
--- C# parte 3/ByteBank/ByteBank/Funcionarios/Funcionario.cs
+++ C# parte 3/ByteBank/ByteBank/Funcionarios/Funcionario.cs
@@ -7,15 +7,25 @@ using System.Threading.Tasks;
 
 namespace ByteBank.Funcionarios
 {
-    public class Funcionario
+    public abstract class Funcionario
     {
-        public string Nome { get; set; }
-        public string CPF { get; set; }
-        public double Salario { get; set; }
 
-        public double GetBonificacao()
+        public int TotalDeFuncionarios { get; private set; }
+        public string Nome { get; set; }
+        public string CPF { get; private set; }
+        public double Salario { get; protected set; }
+
+        public Funcionario(double salario, string cpf)
         {
-            return Salario * 0.10;
+            Salario = salario;
+            CPF = cpf;
+            TotalDeFuncionarios++;
+
+            Console.WriteLine("Construtor de FUNCIONARIO");
         }
+
+        public abstract double GetBonificacao();
+
+        public abstract void AumentarSalario();
     }
 }
