9c9bcc4fe396efbf19994d7aedc9e2e2e2843ad5
Lucas Vieira de Oliveira Cardoso <lucas.v.cardoso@gmail.com>
2020-07-22 05:55:06 -0300

    Iniciando curso C# parte 4 - Excecoes

---
+++ C# parte 3/ByteBank/ByteBank/Funcionarios/FuncionarioAutenticavel.cs
@@ -0,0 +1,20 @@
+﻿using ByteBank.Sistemas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Funcionarios
+{
+    public abstract class FuncionarioAutenticavel : Funcionario, IAutenticavel
+    {
+        public FuncionarioAutenticavel(double salario, string cpf) : base(salario, cpf)
+        {
+        }
+
+        public string Senha { get; set; }
+
+        public bool Autenticar(string senha) => Senha == senha;
+    }
+}
