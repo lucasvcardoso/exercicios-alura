cbe21c72ae85f5d1f0787cddfe4a2ef138eec634
Lucas Vieira de Oliveira Cardoso <lucas.v.cardoso@gmail.com>
2020-07-22 06:41:33 -0300

    C# parte 4 - finalizada aula 2

--- C# parte 4/csharpexceptions-projetoInicial/ByteBank/Program.cs
+++ C# parte 4/csharpexceptions-projetoInicial/ByteBank/Program.cs
@@ -10,7 +10,45 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Metodo();
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Não é possível dividir por zero!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+            
+
             Console.ReadLine();
+        }
+
+        static void Metodo()
+        {
+            TestaDivisao(0);
+        }
+
+        private static void TestaDivisao(int divisor)
+        {
+            Dividir(10, divisor);
+        }
+
+        public static int Dividir(int numero, int divisor)
+        {
+            try
+            {
+                return numero / divisor;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Exceção com numero=" + numero + " e divisor=" + divisor);
+                throw;
+            }
         }
     }
 }
