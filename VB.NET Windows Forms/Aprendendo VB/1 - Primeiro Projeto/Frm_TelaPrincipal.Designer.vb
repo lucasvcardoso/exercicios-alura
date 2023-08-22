4665d03f5e7a3148d5a24e49fb0b22a905804f70
Lucas Cardoso <lucas.v.cardoso@gmail.com>
2023-08-22 10:02:26 -0300

    VB.NET Windows Forms I - Finalizada aula 1, implementando projeto da aula 2.

---
+++ VB.NET Windows Forms/Aprendendo VB/1 - Primeiro Projeto/Frm_TelaPrincipal.Designer.vb
@@ -0,0 +1,51 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Frm_TelaPrincipal
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Btm_Principal = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Btm_Principal
+        '
+        Me.Btm_Principal.Location = New System.Drawing.Point(151, 102)
+        Me.Btm_Principal.Name = "Btm_Principal"
+        Me.Btm_Principal.Size = New System.Drawing.Size(128, 54)
+        Me.Btm_Principal.TabIndex = 0
+        Me.Btm_Principal.Text = "Clique aqui"
+        Me.Btm_Principal.UseVisualStyleBackColor = True
+        '
+        'Frm_TelaPrincipal
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(446, 289)
+        Me.Controls.Add(Me.Btm_Principal)
+        Me.Name = "Frm_TelaPrincipal"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Aplicação Alô Mundo"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Btm_Principal As Button
+End Class
---
+++ VB.NET Windows Forms/Aprendendo VB/2-CriandoVariaveis/Frm_TelaPrincipal.vb
@@ -0,0 +1,14 @@
+﻿Public Class Frm_TelaPrincipal
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+
+    End Sub
+
+    Private Sub Btm_Principal_Click(sender As Object, e As EventArgs) Handles Btm_Principal.Click
+
+    End Sub
+End Class
